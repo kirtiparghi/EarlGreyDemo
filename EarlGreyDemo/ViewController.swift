7483acab0fdc7a782b0df67897936d86cee0efb3
Kirti Parghi <kirtiparghi@gmail.com>
2019-07-12 14:02:20 -0400

    do project setup

---
+++ EarlGreyDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EarlGreyDemo
+//
+//  Created by Kirti Parghi on 7/12/19.
+//  Copyright © 2019 Kirti Parghi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
